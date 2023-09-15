793acafd82bdb3ae8ec0ee3df386e8bd313463a0
Jeroen Nelen <jwnelen@gmail.com>
2023-09-15 16:23:36 +0200

    added some search functionality

--- src/components/podcast.jsx
+++ src/components/podcast.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 // import { HeartIcon } from "@heroicons/react/24/solid";
 import { HeartIcon } from "@heroicons/react/24/outline";
 
 const PodcastView = ({ podcast, isLiked }) => {
-  console.log(podcast);
+  const [liked, setLiked] = useState(isLiked);
+
   return (
     <div className="flex p-2">
-      <img className="w-40 rounded" src={podcast.imageUrl}></img>
-      <div className="text-left p-2 ">
-        <h1 className="font-bold">{podcast.name}</h1>
+      {podcast.imageUrl ? (
+        <img
+          className="w-40 h-40 rounded object-contain"
+          src={podcast.imageUrl}
+        ></img>
+      ) : (
+        <div className="px-20 h-40 flex align-middle font-bold justify-center content-center rounded bg-gray-100"></div>
+      )}
+      <div className="text-left mx-4">
+        <h1 className="font-bold">{podcast.name || ""}</h1>
         <p>{podcast.description}</p>
       </div>
-      <div>
-        {isLiked ? (
+      <div className="" onClick={() => setLiked(!liked)}>
+        {liked ? (
           <HeartIcon className="h-6 w-6 text-red-500 fill-red-500" />
         ) : (
           <HeartIcon className="h-6 w-6" />
